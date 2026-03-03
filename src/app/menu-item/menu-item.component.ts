6bb6e5f45dfc4b4dd8ef54ab1ee224534dcba9fb
Luca324 <tanya.mosol@yandex.ru>
2026-03-04 00:27:40 +0300

    menu item

--- src/app/menu-item/menu-item.component.ts
+++ src/app/menu-item/menu-item.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-menu-item',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './menu-item.component.html',
   styleUrl: './menu-item.component.css'
 })
 export class MenuItemComponent {
+  @Input() link: string = ''
+  @Input() text: string = ''
+  isActive: boolean = false
 
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isActive = this.router.url === this.link
+    });
+  }
 }
